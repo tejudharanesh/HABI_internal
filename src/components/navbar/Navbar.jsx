9340885cc4a7d1558281578e2994907cb464d2f1
tejudharanesh <tejudharanesh1234@gmail.com>
2024-09-12 15:56:10 +0530

    created navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import SelectionIndicator from "./SelectionIndicator";
 
@@ -11,12 +11,19 @@ import team from "../../assets/svg/team.svg";
 import vendors from "../../assets/svg/vendors.svg";
 import logo from "../../assets/habi_logo.png";
 import logout from "../../assets/svg/logout.svg";
+import hamberger from "../../assets/svg/burger.svg";
+import close from "../../assets/svg/close.svg";
 
-const Navbar = ({ isExpanded }) => {
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const location = useLocation();
   const [selected, setSelected] = useState(
     location.pathname.split("/")[2] || "home"
   );
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   const buttons = [
     {
@@ -78,55 +85,78 @@ const Navbar = ({ isExpanded }) => {
   ];
 
   return (
-    <nav
-      className={`bg-layoutColor sticky h-screen w-auto border-r-2 z-10 md:z-10`}
-    >
-      <div className="flex flex-col justify-between h-screen">
-        <div className={`hidden md:flex justify-center px-4 items-center py-4`}>
-          <img src={logo} alt="Logo" className={`ml-3 w-10 h-10`} />
-        </div>
+    <div className="flex">
+      {/* Side Navbar */}
+      <div
+        className={`fixed top-0 left-0 h-full w-auto bg-layoutColor text-white transform ${
+          isOpen ? "translate-x-0" : "-translate-x-full"
+        } transition-transform duration-300 ease-in-out md:translate-x-0`}
+      >
+        <nav
+          className={`bg-layoutColor sticky w-auto border-r-2 z-10 pt-10 md:pt-0`}
+        >
+          <div className="flex flex-col justify-between h-screen">
+            <div className={`flex justify-center px-4 items-center py-4`}>
+              <img
+                src={logo}
+                alt="Logo"
+                className={`ml-3 w-10 h-10 hidden md:inline`}
+              />
+            </div>
 
-        <div className="flex md:flex-col w-auto gap-y-4 h-screen relative">
-          {buttons.map((button) => (
-            <div
-              key={button.id}
-              className={`relative w-auto ${
-                button.id === "logout" ? "absolute top-60" : ""
-              }`}
-            >
-              <Link
-                to={button.link}
-                className={`pl-4 pr-7 py-2 flex flex-col md:flex-row md:space-x-2 hover:bg-primaryO w-full rounded-l-3xl ${
-                  selected === button.id ? " bg-primaryO" : "bg-inherit"
-                }`}
-                onClick={() => setSelected(button.id)}
-              >
-                <img
-                  src={button.icon}
-                  alt={button.alt}
-                  className={`w-6 h-6 md:ml-4 ${
-                    selected === button.id
-                      ? "text-primary fill-primary"
-                      : "#000000"
-                  }`}
-                />
-                <span
-                  className={`hidden lg:flex  font-[18px]  ${
-                    selected === button.id ? "text-primary" : "text-black"
+            <div className="flex flex-col w-auto gap-y-3 h-screen relative">
+              {buttons.map((button) => (
+                <div
+                  key={button.id}
+                  className={`relative w-auto ${
+                    button.id === "logout" ? "absolute top-60" : ""
                   }`}
                 >
-                  {button.label}
-                </span>
-              </Link>
-              {selected === button.id && (
-                <div className="absolute top-0 left-0 right-0 h-1 bg-teal-500 md:hidden" />
-              )}
-              <div>{selected === button.id && <SelectionIndicator />}</div>
+                  <Link
+                    to={button.link}
+                    className={`px-7 md:px-3  lg:pl-3 lg:pr-6 py-2.5 flex flex-row space-x-2 hover:bg-primaryO w-full ${
+                      selected === button.id ? " bg-primaryO" : "bg-inherit"
+                    }`}
+                    onClick={() => setSelected(button.id)}
+                  >
+                    <img
+                      src={button.icon}
+                      alt={button.alt}
+                      className={`w-6 h-6 md:ml-6 ${
+                        selected === button.id
+                          ? "text-primary fill-primary"
+                          : "#000000"
+                      }`}
+                    />
+                    <span
+                      className={`md:hidden lg:flex  font-[18px]  ${
+                        selected === button.id ? "text-primary" : "text-black"
+                      }`}
+                    >
+                      {button.label}
+                    </span>
+                  </Link>
+
+                  <div>{selected === button.id && <SelectionIndicator />}</div>
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
+          </div>
+        </nav>
       </div>
-    </nav>
+
+      {/* Hamburger Button */}
+      <button
+        className="md:hidden fixed top-0 left-2 z-50 bg-layoutColor w-20 border-none"
+        onClick={toggleMenu}
+      >
+        <img
+          src={isOpen ? close : hamberger}
+          alt=""
+          className="w-10 p-0 h-10"
+        />
+      </button>
+    </div>
   );
 };
 
--- src/components/navbar/SelectionIndicator.jsx
+++ src/components/navbar/SelectionIndicator.jsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const SelectionIndicator = () => (
   <div className="selection-indicator">
-    <div className="absolute left-0 top-0 bottom-0 w-1 bg-primary hidden md:flex rounded-r-md"></div>
-    <div className="absolute left-1 top-0 bottom-0 w-3 bg-layoutColor hidden md:flex"></div>
+    <div className="absolute left-0 top-0 bottom-0 w-1 bg-primary flex rounded-r-md"></div>
+    <div className="absolute left-1 top-0 bottom-0 w-3 bg-layoutColor flex"></div>
   </div>
 );
 
