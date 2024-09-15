cc27e56b9905897cf4f48be36d77581b5a1be025
tejudharanesh <tejudharanesh1234@gmail.com>
2024-09-15 15:00:21 +0530

    added customised screen sizes

--- src/components/Admin/Task.jsx
+++ src/components/Admin/Task.jsx
@@ -5,7 +5,7 @@ function Task({ title, tasks }) {
   return (
     <div className="mb-6">
       <h2 className="text-lg font-semibold mb-4 text-black">{title}</h2>
-      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-3">
+      <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-3">
         {tasks.map((task, id) => (
           <Taskcard key={id} task={task} />
         ))}
