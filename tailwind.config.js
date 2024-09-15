f6bed89a5f664f1b392a5439720720edbc322901
tejudharanesh <tejudharanesh1234@gmail.com>
2024-09-15 12:29:36 +0530

    added customised screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,10 @@ export default {
         md: "768px",
         // => @media (min-width: 768px) { ... }
 
-        lg: "1030px",
+        lg: "1024px",
         // => @media (min-width: 1024px) { ... }
 
-        xl: "1280px",
+        xl: "1320px",
         // => @media (min-width: 1280px) { ... }
 
         "2xl": "1536px",
