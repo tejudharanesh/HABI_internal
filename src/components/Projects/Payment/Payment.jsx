89698db4bf84a29e6619d562a26e6209afb43f88
tejudharanesh <tejudharanesh1234@gmail.com>
2024-09-23 13:54:47 +0530

    added priority

--- src/components/Projects/Payment/Payment.jsx
+++ src/components/Projects/Payment/Payment.jsx
@@ -1,12 +1,76 @@
 import React from "react";
+import PaymentBlock from "./PaymentBlock";
+import PaymentSummary from "./PaymentSummary";
+import Header from "../../header/Header";
+const paymentStages = [
+  {
+    stage: "Stage 01",
+    title: "Acceptance of Proposal",
+    cost: "01% of the Total cost",
+    stages: [],
+    week: "1st Week",
+    date: "25 May 2024",
+  },
+  {
+    stage: "Stage 02",
+    title: "Mobilization Advance",
+    cost: "20% of the Total cost",
+    stages: ["Site Marking", "Excavation", "Foundation", "Upto Plinth Level"],
+    week: "2nd Week",
+    date: "",
+    dueDate: "22 June",
+  },
+  {
+    stage: "Stage 03",
+    title: "Completion of Slab",
+    cost: "30% of the Total cost",
+    stages: ["Ground Floor", "First Floor"],
+    week: "9th - 23rd Week",
+    date: "",
+    dueDate: "22 June",
+  },
+  {
+    stage: "Stage 04",
+    title: "Completion of Internal Work",
+    cost: "30% of the Total cost",
+    stages: [
+      "Ground Floor",
+      "First Floor",
+      "Second Floor",
+      "Third Floor",
+      "Forth Floor",
+    ],
+    week: "9th - 23rd Week",
+    date: "",
+    dueDate: "22 June",
+  },
+  // Add more stages as needed
+];
 
+const currentStage = 3;
 function Payment() {
+  const paidSegments = 1;
+  const totalSegments = 7;
+  const segmentAngle = 360 / totalSegments;
+  const paidAngle = paidSegments * segmentAngle;
   return (
     <div className="min-h-screen flex flex-col items-center bg-layoutColor font-poppins">
       <div
-        className={`flex flex-col bg-layoutColor pl-2 md:px-2 h-auto w-screen md:pl-24 lg:pl-40`}
+        className={`flex flex-col bg-layoutColor pl-2 md:px-2 h-auto w-screen md:pl-24  lg:pl-40`}
       >
-        <h1></h1>
+        <Header />
+        <div className="flex flex-col md:pl-10 md:pr-60 lg:pr-[40%] lg:pl-[15%] xl:px-96 xl:mr-60">
+          {paymentStages.map((stages, index) => (
+            <PaymentBlock
+              key={index}
+              data={stages}
+              currentStage={currentStage}
+              stageNumber={index + 1}
+            />
+          ))}
+
+          <PaymentSummary paidAngle={paidAngle} />
+        </div>
       </div>
     </div>
   );
---
+++ src/components/Projects/Payment/PaymentBlock.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import done from "../../../assets/images/done1.png";
+import circle from "../../../assets/images/circle.png";
+
+function PaymentBlock({ data, currentStage, stageNumber }) {
+  const renderStatus = () => {
+    if (currentStage > stageNumber) {
+      return (
+        <div>
+          <div className="absolute top-6 right-14 h-full border-l-2  border-black border-dashed"></div>
+
+          <div className="absolute top-5 right-11 rounded-full">
+            <img src={done} alt="done" />
+          </div>
+          <div className="absolute top-14 right-2">
+            <h2 className="bg-layoutColor p-1">{data.date}</h2>
+          </div>
+        </div>
+      );
+    } else if (currentStage === stageNumber) {
+      return (
+        <div>
+          <div className="absolute top-6 right-14 h-full border-l-2 border-dashed"></div>
+
+          <div className="absolute top-5 right-4 rounded-full">
+            <button className="bg-primary text-white p-2 rounded-lg">
+              Notify User
+            </button>
+          </div>
+          <div className="absolute top-16 right-2">
+            <h2 className="bg-layoutColor p-1">Due on {data.dueDate}</h2>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <div className="absolute top-6 right-14 h-full border-l-2 border-dashed"></div>
+
+          <div className="absolute top-5 right-11 rounded-full">
+            <img src={circle} alt="done" className="h-7 w-7" />
+          </div>
+        </div>
+      );
+    }
+  };
+  return (
+    <div className="relative">
+      <div>
+        <h3 className="text-lg m-1">{data.stage}</h3>
+        <p className="text-black font-semibold text-[16px] m-1">{data.title}</p>
+
+        {data.stages.map((stage, index) => (
+          <p className="text-xs ml-1">{stage}</p>
+        ))}
+
+        {/* sub lists will be in p tag*/}
+        <p className="text-md text-black m-1">{data.cost}</p>
+        <p className="text-gray-400 m-1 text-sm">{data.week}</p>
+      </div>
+      {renderStatus()}
+
+      <hr className="border-[1px] mt-5 w-4/6" />
+    </div>
+  );
+}
+
+export default PaymentBlock;
